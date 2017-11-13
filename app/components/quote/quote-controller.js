e2a6fa3cc1b95f581546aa95e9af77e2fa9ec068
Morgan Waters <morangwaters@gmail.com>
2017-11-13 14:03:59 -0700

    setting up draw

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -1,8 +1,22 @@
 function QuoteController() {
 
 	var qs = new QuoteService()
+	var quoteElem = document.getElementById('quote');
+
 
 	qs.getQuote(function (quote) {
 		console.log('What is the quote', quote)
 	})
+
+	function drawQuote(quote) {
+		var template = ''
+		template += `
+	<div>
+	<h3>${quote.quote}</h3>
+	</div>
+
+	`
+
+	}
+	qs.getQuote(drawQuote)
 }
--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,16 +1,30 @@
 function WeatherController() {
 	var ws = new WeatherService();
 
-	function drawWeather(weather)
-	console.log(weather);
-	var weatherElem = document.getElementById('weather');
-	ws.getWeather(drawWeather)
+	var weatherElem = document.getElementById('weather')
+
+	function getWeather() {
+		weatherService.getWeather(drawWeather)
+	}
+
+	function drawWeather(getWeather) {
+		console.log(weather);
+		for (var i = 0; i < weather.length; i++) {
+			var weather = weather[i];
+			var template = ''
+			template += `
+			<div>
+			<h3>${weather}</h3>
+			</div>
+			
+			`
+		}
+	}
 
 	function convertTemp(weather) {
 		var f = ((K - 273.15) * 1.8) + 32
 		var k = 0
 		var c = K - 273.15
 	}
-
-
+	weatherElem.innerHTML = template
 }
