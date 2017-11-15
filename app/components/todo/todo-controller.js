76cb4fabfe0fc68c5a8158bff09c9589cc58c193
Morgan Waters <morangwaters@gmail.com>
2017-11-15 15:49:14 -0700

    Last strech of todo updates

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,59 +1,37 @@
 function TodoController() {
-	// new up the TodoService that has already been configured for your use
-	// You will need four methods
-	// getTodos should request your api/todos and give an array of todos to your callback fn
-	// addTodo takes in a todo and posts it to the server
-	// toggleTodoStatus takes in a todo marks its status as completed and puts it to the server
-	// removeTodo takes in a todoId and sends a delete request to the server
-	// **** HINT: Everytime you make a change to any todo don't forget to get the todo list again
 	var todoService = new TodoService()
-
-	// Use this getTodos function as your callback for all other edits
 	function getTodos() {
-		//FYI DONT EDIT ME :)
 		todoService.getTodos(draw)
 	}
-	var todoElem = document.getElementById('todo');
-
 	function draw(todos) {
-		//WHAT IS MY PURPOSE?
-		//BUILD YOUR TODO TEMPLATE HERE
+		var formElem = document.getElementById('todo');
 		var template = ''
-		//DONT FORGET TO LOOP
-		for (var i = 0; i < todo.length; i++) {
-			var todo = todo[i];
-
+		for (var i = 0; i < todos.length; i++) {
+			var todo = todos[i];
+			template += `
+			<div class="col-md-3">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3>${i + 1}. ${todo.todo}
+						<button type="submit" onclick="app.components.todo-controller.removeTodo(${i})" class="btn btn-default">Remove</button>
+					</h3>
+				</div>
+			</div>
+		</div>
+			`
 		}
-		todoElem.innerHTML = template
-	}
-
-	this.addTodoFromForm = function addTodoFromForm(e) {
-		e.preventDefault() // <-- hey this time its a freebie don't forget this
-		// TAKE THE INFORMATION FORM THE FORM
-		var form = e.target
-		todoService.addTodo(form, getTodos)
-		todosFormElem.classList.toggle('hidden', true)
-		// DONT FORGET TO BUILD YOUR TODO OBJECT
-
-		//PASSES THE NEW TODO TO YOUR SERVICE
-		//DON'T FORGET TO REDRAW THE SCREEN WITH THE NEW TODO
-		//YOU SHOULDN'T NEED TO CHANGE THIS
+		formElem.innerHTML = template
+		this.addTodoFromForm = function addTodoFromForm(e) {
+			e.preventDefault()
+			var form = e.target
+		}
 		todoService.addTodo(todo, getTodos)
-		//^^^^^^^ EXAMPLE OF HOW TO GET YOUR TOODOS AFTER AN EDIT
 	}
-
 	this.toggleTodoStatus = function (todoId) {
-		// asks the service to edit the todo status
 		todoService.toggleTodoStatus(todoId, getTodos)
-		// YEP THATS IT FOR ME
 	}
-
-	this.removeTodo = function (todoId) {
-		// ask the service to run the remove todo with this id
-
-		// ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
+	this.removeTodo = function removeTodo(index) {
+		todoService.removeTodo(index, getTodos)
 	}
-
-	// IF YOU WANT YOUR TODO LIST TO DRAW WHEN THE PAGE FIRST LOADS WHAT SHOULD YOU CALL HERE???
-
+	getTodos()
 }
--- app/components/todo/todo-service.js
+++ app/components/todo/todo-service.js
@@ -1,38 +1,24 @@
 function TodoService() {
-	// A local copy of your todos
-	var todoList = []
 	var baseUrl = 'https://inspire-server.herokuapp.com/api/todos/morganwaters'
-
 	function logError(err) {
 		console.error('UMM SOMETHING BROKE: ', err)
-		//CAN YOU NOTIFY THE USER IF SOMETHING BREAKS? 
-		//do this without breaking the controller/service responsibilities
 	}
-
+	var id = 0
 	this.getTodos = function (draw) {
 		$.get(baseUrl)
-			.then(function (res) { // <-- WHY IS THIS IMPORTANT????
-
+			.then(function (res) {
+				todos = res
+				draw(todos)
 			})
 			.fail(logError)
 	}
-
-	this.addTodo = function (todo) {
-		// WHAT IS THIS FOR???
+	this.addTodo = function (todo, getTodos) {
 		$.post(baseUrl, todo)
-			.then(function (res) { // <-- WHAT DO YOU DO AFTER CREATING A NEW TODO?
-
+			.then(function (res) {
 			})
 			.fail(logError)
 	}
-
 	this.toggleTodoStatus = function (todoId) {
-		// MAKE SURE WE THINK THIS ONE THROUGH
-		//STEP 1: Find the todo by its index **HINT** todoList
-
-		//STEP 2: Change the completed flag to the opposite of what is is **HINT** todo.completed = !todo.completed
-
-		//STEP 3: Here is that weird Ajax request because $.put doesn't exist
 		$.ajax({
 			method: 'PUT',
 			contentType: 'application/json',
@@ -40,14 +26,15 @@ function TodoService() {
 			data: JSON.stringify(YOURTODOVARIABLEHERE)
 		})
 			.then(function (res) {
-				//DO YOU WANT TO DO ANYTHING WITH THIS?
 			})
 			.fail(logError)
 	}
-
-	this.removeTodo = function () {
-		// Umm this one is on you to write.... It's also unique, like the ajax call above. The method is a DELETE
-
+	this.removeTodo = function (index, getTodos) {
+		$.ajax({
+			method: 'DELETE',
+			url: baseUrl + '/' + index,
+		})
+			.then(getTodos)
+			.fail(logError)
 	}
-
 }
